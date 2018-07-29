31f7ec980aec6af8f1dec6b936d7bce886e0d2a9
Maura Aldera <37031693+mauraldera@users.noreply.github.com>
2018-07-29 15:49:53 +0700

    added payment feature

--- routes/index.js
+++ routes/index.js
@@ -206,5 +206,56 @@ router.get('/remove/:id', function (req,res,next) {
     res.redirect('/shopping-cart');
 });
 
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    var errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
 
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    
+    var stripe = require("stripe")(
+        "sk_test_LsMY1pTXWKnPyOc3v7jxdGkJ"
+    );
+
+    stripe.charges.create({
+        amount: cart.totalPrice * 100,
+        currency: "usd",
+        source: req.body.stripeToken, // obtained with Stripe.js
+        description: "Test Charge"
+    }, function(err, charge) {
+        if (err) {
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+        var order = new Order({
+            user: req.user,
+            cart: cart,
+            address: req.body.address,
+            name: req.body.name,
+            paymentId: charge.id
+        });
+        order.save(function(err, result) {
+            req.flash('success', 'Successfully bought product!');
+            req.session.cart = null;
+            res.redirect('/');
+        });
+    }); 
+});
 module.exports = router;
+
+function isLoggedIn (req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
